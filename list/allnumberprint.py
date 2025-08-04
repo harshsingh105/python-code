e3d9639cd8ab1ee937a1332329385d7e91b3b59e
harshsingh105 <harshsingh22037@gmail.com>
2025-08-04 22:14:42 +0530

    did some coding problems of for loop and list

---
+++ list/allnumberprint.py
@@ -0,0 +1,3 @@
+list = [8,9,10,11,12,13,14,15,16,17,18]
+list_length = len(list)
+print(len(list))
---
+++ list/printsumofallnumbers.py
@@ -0,0 +1,8 @@
+numbers = [4, 7, 2, 9, 8498,5646,456,4356,547456,676654,5666]
+total = 0
+
+for sum in numbers:
+    total += sum
+
+print("Sum of all numbers:", total)
+
